a2783339604f05867bca760e36abb9818bc6e31b
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-14 12:09:18 +0300

    Add test for the app component

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,14 @@
+import {
+  render, screen,
+} from '@testing-library/react';
+import { Provider } from 'react-redux';
+import App from '../App';
+import '@testing-library/jest-dom/extend-expect';
+import store from '../store';
+
+describe('app routes', () => {
+  it('app elements', () => {
+    render(<Provider store={store}><App /></Provider>);
+    expect(screen.getByTestId('home')).toBeInTheDocument();
+  });
+});
